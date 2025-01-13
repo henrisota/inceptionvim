d418b74167723513bf540d914d10af99be959b8c
Henri Sota <sotahenry@gmail.com>
2025-01-13 16:21:51 +0100

    disable dynamic providers

--- packages/inceptionvim/options.nix
+++ packages/inceptionvim/options.nix
@@ -7,6 +7,12 @@
   globals = {
     mapleader = " ";
     maplocalleader = ",";
+
+    # Disable providers
+    loaded_node_provider = 0;
+    loaded_perl_provider = 0;
+    loaded_python3_provider = 0;
+    loaded_ruby_provider = 0;
   };
 
   opts = {
