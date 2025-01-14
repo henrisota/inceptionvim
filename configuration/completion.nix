442c231018bf871153bd1a331074b892ee0da541
Henri Sota <sotahenry@gmail.com>
2025-01-14 23:37:45 +0100

    set behavior of cmp mappings and default configuration for cmp windows

--- configuration/completion.nix
+++ configuration/completion.nix
@@ -13,28 +13,53 @@
       settings = {
         mapping = {
           "<C-e>" = "cmp.mapping.close()";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<CR>" = "cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace })";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i', 's'})";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i', 's'})";
         };
 
         performance = {
           max_view_entries = 25;
         };
 
-        sources =
+        sources = let
+          get_bufnrs = {
+            __raw = ''
+              function()
+                local buf_size_limit = 1024 * 1024
+                local bufs = vim.api.nvim_list_bufs()
+                local valid_bufs = {}
+                for _, buf in ipairs(bufs) do
+                  if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf)) < buf_size_limit then
+                    table.insert(valid_bufs, buf)
+                  end
+                end
+                return valid_bufs
+              end
+            '';
+          };
+        in
           (lib.optionals config.plugins.lsp.enable [
             {
               name = "nvim_lsp";
               priority = 1000;
+              option = {
+                inherit get_bufnrs;
+              };
             }
             {
               name = "nvim_lsp_signature_help";
               priority = 1000;
+              option = {
+                inherit get_bufnrs;
+              };
             }
             {
               name = "nvim_lsp_document_symbol";
               priority = 1000;
+              option = {
+                inherit get_bufnrs;
+              };
             }
           ])
           ++ (lib.optional config.plugins.treesitter.enable
@@ -46,13 +71,20 @@
             {
               name = "buffer";
               priority = 500;
-              option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+              option = {
+                inherit get_bufnrs;
+              };
             }
             {
               name = "path";
               priority = 300;
             }
           ];
+
+        window = {
+          completion.__raw = ''cmp.config.window.bordered()'';
+          documentation.__raw = ''cmp.config.window.bordered()'';
+        };
       };
     };
 
@@ -69,11 +101,8 @@
           buffer = "[Buffer]";
           calc = "[Calc]";
           cmdline = "[Cmdline]";
-          codeium = "[Codeium]";
           emoji = "[Emoji]";
           git = "[Git]";
-          luasnip = "[Snippet]";
-          neorg = "[Neorg]";
           nvim_lsp = "[LSP]";
           nvim_lua = "[API]";
           path = "[Path]";
