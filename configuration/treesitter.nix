81af5401792407df1fd2ed7f95148f97fe54e728
Henri Sota <sotahenry@gmail.com>
2025-02-24 22:03:48 +0100

    add prelimiary textobjects keymappings

--- configuration/treesitter.nix
+++ configuration/treesitter.nix
@@ -59,12 +59,43 @@
       move = {
         enable = false;
         setJumps = false;
+
+        gotoNextStart = {
+          "]m" = "@function.outer";
+          "]]" = "@class.outer";
+        };
+        gotoNextEnd = {
+          "]M" = "@function.outer";
+          "][" = "@class.outer";
+        };
+        gotoPreviousStart = {
+          "[m" = "@function.outer";
+          "[[" = "@class.outer";
+        };
+        gotoPreviousEnd = {
+          "[M" = "@function.outer";
+          "[]" = "@class.outer";
+        };
       };
 
       select = {
         enable = false;
         includeSurroundingWhitespace = false;
-        lookahead = false;
+        lookahead = true;
+
+        keymaps = {
+          "aa" = "@parameter.outer";
+          "ia" = "@parameter.inner";
+          "af" = "@function.outer";
+          "if" = "@function.inner";
+          "ac" = "@class.outer";
+          "ic" = "@class.inner";
+          "ii" = "@conditional.inner";
+          "ai" = "@conditional.outer";
+          "il" = "@loop.inner";
+          "al" = "@loop.outer";
+          "at" = "@comment.outer";
+        };
       };
 
       swap = {
