c076b17e2dd5791bcd396463c9b73b60ae3156ea
Henri Sota <sotahenry@gmail.com>
2025-02-25 21:41:15 +0100

    configure telescope fzf behavior

--- configuration/telescope.nix
+++ configuration/telescope.nix
@@ -8,7 +8,15 @@
       enable = true;
 
       extensions = {
-        fzf-native.enable = true;
+        fzf-native = {
+          enable = true;
+          settings = {
+            fuzzy = true;
+            override_generic_sorter = true;
+            override_file_sorter = true;
+            case_mode = "smart_case";
+          };
+        };
         project.enable = false;
         undo.enable = false;
       };
