e75e4d012e7919a36f7c95ffe838652b151ae1f6
Henri Sota <sotahenry@gmail.com>
2025-02-25 21:18:35 +0100

    add format filter and replace patterns for noice

--- configuration/editor.nix
+++ configuration/editor.nix
@@ -397,6 +397,33 @@
           };
         };
 
+        format = {
+          filter = {
+            pattern = [
+              ":%s*%%s*s:%s*"
+              ":%s*%%s*s!%s*"
+              ":%s*%%s*s/%s*"
+              "%s*s:%s*"
+              ":%s*s!%s*"
+              ":%s*s/%s*"
+            ];
+            icon = "";
+            lang = "regex";
+          };
+          replace = {
+            pattern = [
+              ":%s*%%s*s:%w*:%s*"
+              ":%s*%%s*s!%w*!%s*"
+              ":%s*%%s*s/%w*/%s*"
+              "%s*s:%w*:%s*"
+              ":%s*s!%w*!%s*"
+              ":%s*s/%w*/%s*"
+            ];
+            icon = "󱞪";
+            lang = "regex";
+          };
+        };
+
         health.checker = true;
 
         lsp = {
