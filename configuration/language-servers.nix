eaacb6aabf68ffe9aa6fd68d5dbcff89602c9524
Henri Sota <sotahenry@gmail.com>
2025-02-26 17:09:40 +0100

    define preliminary lsp buf actions

--- configuration/language-servers.nix
+++ configuration/language-servers.nix
@@ -110,6 +110,51 @@
     # https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
     lsp = {
       enable = true;
+
+      keymaps = {
+        lspBuf = {
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+          "<leader>ca" = {
+            action = "code_action";
+            desc = "Code Action";
+          };
+          "<C-k>" = {
+            action = "signature_help";
+            desc = "Signature Help";
+          };
+        };
+      };
     };
 
     which-key = lib.mkIf config.plugins.which-key.enable {
