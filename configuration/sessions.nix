7220afc82c74b68bededd0d19942ef36e6a8c86b
Henri Sota <sotahenry@gmail.com>
2025-02-28 12:50:46 +0100

    handle neotree in auto-session to avoid buffer issues on restore

--- configuration/sessions.nix
+++ configuration/sessions.nix
@@ -1,4 +1,8 @@
-_: {
+{
+  lib,
+  config,
+  ...
+}: {
   plugins = {
     auto-session = {
       enable = true;
@@ -9,6 +13,10 @@ _: {
         auto_save = true;
         auto_restore_last_session = true;
         use_git_branch = true;
+
+        pre_save_cmds = lib.optional config.plugins.neo-tree.enable "Neotree close";
+        pre_restore_cmds = [];
+        post_restore_cmds = lib.optional config.plugins.neo-tree.enable "Neotree filesystem show";
       };
     };
   };
